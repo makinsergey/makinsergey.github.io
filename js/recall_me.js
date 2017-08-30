5147c6d9c91fea70594bf6deface676d65431521
makinsergey <suradero@gmail.com>
2017-08-30 17:10:04 +0300

    Add files via upload

--- js/recall_me.js
+++ js/recall_me.js
@@ -1,36 +1,75 @@
-$("#contactForm").submit(function(event){
-  // cancels the form submission
-  event.preventDefault();
-  submitForm();
-});
+/*
+  Jquery Validation using jqBootstrapValidation
+   example is taken from jqBootstrapValidation docs 
+  */
+$(function() {
 
-function submitForm(){
-    // Переменные с данными из формы
-    var name = $("#name").val();
-    var phone = $("#phone").val();
-    $.ajax({
-        type: "POST",
-        url: "process_form.php",
-        data: "name="+ name + "&phone="+ phone,
-        // success : function(text){
-        //     if(text == "success"){
-        //         formSuccess();
-        //     }
-        // }
-        success: function() {  
+ $("input,textarea").jqBootstrapValidation(
+    {
+     preventSubmit: true,
+     submitError: function($form, event, errors) {
+      // something to have when submit produces an error ?
+      // Not decided if I need it yet
+     },
+     submitSuccess: function($form, event) {
+      event.preventDefault(); // prevent default submit behaviour
+       // get values from FORM
+       var name = $("input#name").val();  
+       var phone = $("input#phone").val(); 
+        var firstName = name; // For Success/Failure Message
+           // Check for white space in name for Success/Fail message
+        if (firstName.indexOf(' ') >= 0) {
+  firstName = name.split(' ').slice(0, -1).join(' ');
+         }        
+$.ajax({
+                url: "recall_me.php",
+             type: "POST",
+             data: {name: name, phone: phone},
+             cache: false,
+             success: function() {  
              // Success message
-            $('#success').html("<div class='alert alert-success'>");
-            $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+                $('#success').html("<div class='alert alert-success'>");
+                $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
              .append( "</button>");
-            $('#success > .alert-success')
+               $('#success > .alert-success')
              .append("<strong>Спасибо! Ваше сообщение отправлено.</strong>");
-            $('#success > .alert-success')
-            .append('</div>');
+    $('#success > .alert-success')
+  .append('</div>');
+
+   
     //clear all fields
-            $('#contactForm').trigger("reset");
+    $('#contactForm').trigger("reset");
+
+setTimeout(function() {
+                $("#myModal").modal('hide');
+           }, 2000);
+
         },
-    });
-}
-function  formSuccess(){
-    $( "#msgSubmit").removeClass( "hidden");
-}
+     error: function() { 
+  // Fail message
+   $('#success').html("<div class='alert alert-danger'>");
+             $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+              .append( "</button>");
+             $('#success > .alert-danger').append("<strong>Извините "+firstName+" кажется проблемы на сервере отправки писем...</strong> Вы не могли бы написать напрямую на адрес <a href='arina1611@mail.ru?Subject=Перезвоните мне'>arina1611@mail.ru</a> ? Приносим извинения за это неудобство!");
+          $('#success > .alert-danger').append('</div>');
+  //clear all fields
+  $('#contactForm').trigger("reset");
+      },
+           })
+         },
+         filter: function() {
+                   return $(this).is(":visible");
+         },
+       });
+
+      $("a[data-toggle=\"tab\"]").click(function(e) {
+                    e.preventDefault();
+                    $(this).tab("show");
+        });
+  });
+
+
+/*When clicking on Full hide fail/success boxes */ 
+$('#name').focus(function() {
+     $('#success').html('');
+  });
---
+++ recall_me.php
@@ -0,0 +1,22 @@
+<?php
+	// check if fields passed are empty
+	if(empty($_POST['name'])   ||
+	   empty($_POST['phone']))
+	   {
+	echo "No arguments Provided!";
+	return false;
+	   }
+
+	$name = $_POST['name'];
+	$phone = $_POST['phone'];
+	// create email body and send it 
+	$to = 'nyanvodka@yandex.ru'; // put your email
+	$email_subject = "Вам отправлена форма: $name";
+	$email_body = "Заполнена форма. \n\n".
+	 "Данные отправителя:\n\nИмя: $name \n".
+	 "Телефон: $phone \n".;
+	$headers = "From: contact@mail.com\n";
+	$headers .= "Reply-To:"; 
+	mail($to,$email_subject,$email_body,$headers);
+	return true; 
+?>
