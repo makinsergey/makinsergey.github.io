b7fd98f39a87bbe45a3852520c60b4bac9d583b5
makinsergey <suradero@gmail.com>
2017-08-30 17:39:03 +0300

    Add files via upload

--- recall_me.php
+++ recall_me.php
@@ -11,7 +11,7 @@
 	$phone = $_POST['phone'];
 	// create email body and send it 
 	$to = 'nyanvodka@yandex.ru'; // put your email
-	$email_subject = "Вам отправлена форма: $name";
+	$email_subject = "Вам отправлена форма";
 	$email_body = "Заполнена форма. \n\n".
 	 "Данные отправителя:\n\nИмя: $name \n".
 	 "Телефон: $phone \n".;
