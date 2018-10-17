ba3c89c2fb9a5ed2a6533730e7ecb56ee4f9cd40
Sergey Makin <srjmckeen@gmail.com>
2018-10-17 15:35:13 +0300

    del

--- ellemebel/wp-content/themes/electro/inc/electro-functions.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Functions used in Electro
- *
- * @since 2.0
- */
-
-require_once get_template_directory() . '/inc/functions/header.php';
-require_once get_template_directory() . '/inc/functions/home.php';
